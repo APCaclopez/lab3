3950c509d142b8c62082dadae82b175b2dec235f
Aliyah Kirstie Lopez <aclopez@student.apc.edu.ph>
2023-02-28 13:17:07 +0800

    connect remote database

---
+++ ci4/app/Views/news/mysql_insert_ci4.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "192.168.150.213";
+$username = "webprogss211";
+$password = "fancyR!ce36";
+$dbname = "webprogss211";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO news VALUES
+(1,'Elvis sighted','elvis-sighted','Elvis was sighted at the Podunk internet cafe. It looked like he was writing a CodeIgniter app.'),
+(2,'Say it isn\'t so!','say-it-isnt-so','Scientists conclude that some programmers have a sense of humor.'),
+(3,'Caffeination, Yes!','caffeination-yes','World\'s largest coffee shop open onsite nested coffee shop for staff only.')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
